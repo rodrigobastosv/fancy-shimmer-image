2239336c1b5a918c978cc5f8053373944a5e67e3
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-12-16 15:04:11 -0300

    More

--- example/lib/data.dart
+++ example/lib/data.dart
@@ -17,7 +17,7 @@ class InfoModel {
   final Color shimmerBackColor;
 }
 
-final data = <InfoModel>[
+final dataRandom = <InfoModel>[
   InfoModel(
     url:
         'https://static.businessinsider.sg/2018/12/12/5c1c90f8e04d6243c7019cf6.png',
@@ -121,5 +121,67 @@ final data = <InfoModel>[
     shimmerBaseColor: R.randomColor(),
     shimmerHighlightColor: R.randomColor(),
     shimmerBackColor: R.randomColor(),
+  ),
+];
+
+final dataDefault = <InfoModel>[
+  InfoModel(
+    url:
+        'https://static.businessinsider.sg/2018/12/12/5c1c90f8e04d6243c7019cf6.png',
+  ),
+  InfoModel(
+    url:
+        'https://66.media.tumblr.com/6a4b9ac513b14406ce53c4ec5bea3244/tumblr_ojghm5engn1vehqeko1_400.jpg',
+  ),
+  InfoModel(
+    url:
+        'https://images.pexels.com/photos/221433/pexels-photo-221433.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+  ),
+  InfoModel(
+    url: 'http://eskipaper.com/images/high-resolution-backgrounds-22.jpg',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPYaZYxi_68IX_oHuNOI2-LQHy_PBWSLYpCWc6MSga842C1Dpe&s',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs6ZykmCD6SlkX7cmLv-0zR1M_iHZczaU8gPlZtEZiii0MFAE_&s',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4a1oj3Do9ajb7ac48K6TV3nPcWbnjZ_RuK91NflYfTnB8KsIlxg&s',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpOwkeMxA8_h55GsTgIrHy_u70_s-Emkra1HmNnkTi3HRDsnq6&s',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK1uGRnmrX8_h2_K8ddvBh2wdYWQNi5s1aSPl_FXPcg5aa7w5hQw&s',
+  ),
+  InfoModel(
+    url:
+        'https://vastphotos.com/files/uploads/photos/10551/beautiful-mountain-photo-l.jpg',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5UplwcFSbcH9J2zjdTButF2ilEZ8vNzg68iYAhLTI0J8k2pKt&s',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_Q_rvjZ5ozyCrjxrOqWByQOE8ywaYGC792Bp0lbNnoIX_u9uJMg&s',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn0jtO81k-PA5QH7yQ3vgNDn1uesE4uCRHzUe6QLrvE24fhiC8tw&s',
+  ),
+  InfoModel(
+    url:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGKYA2425OnOHzz1j7Ikvp6ArBZNvDf1Zlok4LXlfbAZm28k2o&s',
+  ),
+  InfoModel(
+    url:
+        'https://www.bookdesignmadesimple.com/wp-content/uploads/2015/08/Chrysanthemum.jpg',
   ),
 ];
